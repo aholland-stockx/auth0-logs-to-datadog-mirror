fad08d35c1de81293cd2fe60ef021a8bf6db9de6
aholland-stockx <andrewholland@stockx.com>
2020-01-16 10:16:07 -0500

    Correct formatting

--- server/lib/datadog.js
+++ server/lib/datadog.js
@@ -3,45 +3,45 @@ const logger = require('./logger');
 const axios = require('axios');
 
 const metadata = {
-  ddsourcecategory: 'external',
-  ddsource: 'auth0'
+    ddsourcecategory: 'external',
+    ddsource: 'auth0'
 };
 
 const config = {};
 
 function DataDog(server, apiKey, customTags) {
-  if (!apiKey) {
-    throw new Error('API Key is required for DataDog.');
-  }
-
-  config.apiKey = apiKey;
-
-  if (customTags) {
-    const matchedTags = customTags.match(/([^:|^,\W]+):([^,|^\W]+)/g);
-    if (!matchedTags || matchedTags.length < 1) {
-      throw new Error('Custom tags are not formatted properly. Format is comma-separated key:value.');
+    if (!apiKey) {
+        throw new Error('API Key is required for DataDog.');
     }
 
-    metadata.ddtags = customTags;
-  }
+    config.apiKey = apiKey;
+
+    if (customTags) {
+        const matchedTags = customTags.match(/([^:|^,\W]+):([^,|^\W]+)/g);
+        if (!matchedTags || matchedTags.length < 1) {
+            throw new Error('Custom tags are not formatted properly. Format is comma-separated key:value.');
+        }
+
+        metadata.ddtags = customTags;
+    }
 }
 
 DataDog.prototype.log = (log) => {
 
-  console.log(log[0]);
+    console.log(log[0]);
 
-  return axios.post(`https://http-intake.logs.datadoghq.com/v1/input/${config.apiKey}?ddsource=auth2&service=auth2&hostname=accounts.staging.stockx.io`, [log[0]], {
-    headers: {
-      'Content-Type': 'application/json'
-    }
-  }).then(response => {
-    logger.info(`Got success response`);
-    console.log(response.data);
-    console.log(response.status);
-  }).catch(error => {
-    logger.info(`Got error`);
-    logger.info(error);
-  });
+    return axios.post(`https://http-intake.logs.datadoghq.com/v1/input/${config.apiKey}?ddsource=auth2&service=auth2&hostname=accounts.staging.stockx.io`, [log[0]], {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    }).then(response => {
+        logger.info(`Got success response`);
+        console.log(response.data);
+        console.log(response.status);
+    }).catch(error => {
+        logger.info(`Got error`);
+        logger.info(error);
+    });
 };
 
 module.exports = DataDog;
--- server/lib/processLogs.js
+++ server/lib/processLogs.js
@@ -5,47 +5,47 @@ const logger = require('./logger');
 const DataDog = require('./datadog');
 
 module.exports = storage =>
-  (req, res, next) => {
-    const wtBody = (req.webtaskContext && req.webtaskContext.body) || req.body || {};
-    const wtHead = (req.webtaskContext && req.webtaskContext.headers) || {};
-    const isCron = (wtBody.schedule && wtBody.state === 'active') || (wtHead.referer === `${config('AUTH0_MANAGE_URL')}/` && wtHead['if-none-match']);
+    (req, res, next) => {
+        const wtBody = (req.webtaskContext && req.webtaskContext.body) || req.body || {};
+        const wtHead = (req.webtaskContext && req.webtaskContext.headers) || {};
+        const isCron = (wtBody.schedule && wtBody.state === 'active') || (wtHead.referer === `${config('AUTH0_MANAGE_URL')}/` && wtHead['if-none-match']);
 
-    if (!isCron) {
-      return next();
-    }
-
-    const datadog = new DataDog(config('DATADOG_SERVER'), config('DATADOG_API_KEY'), config('DATADOG_CUSTOM_TAGS'));
-
-    const auth0logger = new loggingTools.LogsProcessor(storage, {
-      domain: config('AUTH0_DOMAIN'),
-      clientId: config('AUTH0_CLIENT_ID'),
-      clientSecret: config('AUTH0_CLIENT_SECRET'),
-      batchSize: 100,
-      startFrom: config('START_FROM'),
-      logLevel: config('LOG_LEVEL'),
-      logTypes: config('LOG_TYPES')
-    });
-
-    return auth0logger
-      .run(logs => {
-
-        logger.info('Running');
-        console.log('Running');
-
-        if (!logs) {
-          logger.info('No logs');
-          return Promise.resolve();
+        if (!isCron) {
+            return next();
         }
 
-        logger.info(`Sending ${logs.length} logs to DataDog.`);
+        const datadog = new DataDog(config('DATADOG_SERVER'), config('DATADOG_API_KEY'), config('DATADOG_CUSTOM_TAGS'));
 
-        return datadog.log(logs);
-      })
-      .then(status => {
-        logger.info('Done');
-        console.log('Done');
-        console.log(status);
+        const auth0logger = new loggingTools.LogsProcessor(storage, {
+            domain: config('AUTH0_DOMAIN'),
+            clientId: config('AUTH0_CLIENT_ID'),
+            clientSecret: config('AUTH0_CLIENT_SECRET'),
+            batchSize: 100,
+            startFrom: config('START_FROM'),
+            logLevel: config('LOG_LEVEL'),
+            logTypes: config('LOG_TYPES')
+        });
 
-      })
-      .catch(err => console.log(err));
-  };
+        return auth0logger
+            .run(logs => {
+
+                logger.info('Running');
+                console.log('Running');
+
+                if (!logs) {
+                    logger.info('No logs');
+                    return Promise.resolve();
+                }
+
+                logger.info(`Sending ${logs.length} logs to DataDog.`);
+
+                return datadog.log(logs);
+            })
+            .then(status => {
+                logger.info('Done');
+                console.log('Done');
+                console.log(status);
+
+            })
+            .catch(err => console.log(err));
+    };
