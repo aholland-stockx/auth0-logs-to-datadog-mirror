7ca2cfd790c58dfe7fa434f6f18cc891d9eb36f6
aholland-stockx <andrewholland@stockx.com>
2020-01-27 08:38:39 -0500

    Test single log JSON format

--- server/lib/datadog.js
+++ server/lib/datadog.js
@@ -27,14 +27,18 @@ DataDog.prototype.log = (logs) => {
 
     var test = {"date":"2019-12-25T19:31:05.657Z","type":"feccft","description":"Unauthorized","connection_id":"","client_id":"1EUxegNhKI7UTelEGkaTnl4sljOFXUz5","client_name":null,"ip":"35.160.3.103","user_agent":"Other 0.0.0 / Other 0.0.0","details":{"device_id":"v0:17faf830-274d-11ea-9528-6759a37739cd"},"hostname":"stockx-staging.auth0.com","user_id":"","user_name":"","audience":"https://stockx-staging.auth0.com/api/v2/","scope":null,"log_id":"90020191225193109813000852820998500973574084845960691826","_id":"90020191225193109813000852820998500973574084845960691826","isMobile":false}
 
-    return axios.post(postaddr, test, {
+    var json = $.parseJSON(test)
+    console.log((typeof(json) === 'object'))
+    console.log(json)
+
+    return axios.post(postaddr, json, {
         headers: {
             'Content-Type': 'application/json'
         },
         params: {
             ddsource: config.source,
             hostname: config.hostname,
-            service: 'auth0'
+            service: 'auth9'
         }
     }).then(response => {
         console.log('Worked');
